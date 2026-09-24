d8049befbc5d32cbc9e881ae5b2099ad483ef421
karpuzikov <karpuzikovanton@gmail.com>
2026-09-24 08:03:54 +0300

    Remove duplicated bootstrap launcher code

--- audio-tools/audiochecker-utf8-patch/Install_AudioChecker_UTF8_Patch.bat
+++ audio-tools/audiochecker-utf8-patch/Install_AudioChecker_UTF8_Patch.bat
@@ -18,7 +18,7 @@ if not exist "%ROOT%\achkgui.exe" (
 )
 
 set "AUDIOCHECKER_ROOT=%ROOT%"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "$ErrorActionPreference='Stop'; $root=$env:AUDIOCHECKER_ROOT; $items=@(@('achkgui.exe.manifest','AudioChecker.UTF8.achkgui'),@('Codecs\APE\mac.exe.manifest','AudioChecker.UTF8.Codecs.APE.mac'),@('Codecs\Analyzer\aucdtect.exe.manifest','AudioChecker.UTF8.Codecs.Analyzer.aucdtect'),@('Codecs\FLAC\flac.exe.manifest','AudioChecker.UTF8.Codecs.FLAC.flac'),@('Codecs\LPAC\lpac.exe.manifest','AudioChecker.UTF8.Codecs.LPAC.lpac'),@('Codecs\SHN\shortn32.exe.manifest','AudioChecker.UTF8.Codecs.SHN.shortn32')); $utf8=New-Object Text.UTF8Encoding($false); foreach($item in $items){ $dest=Join-Path $root $item[0]; [IO.Directory]::CreateDirectory([IO.Path]::GetDirectoryName($dest)) ^| Out-Null; $xml='<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + [Environment]::NewLine + '<assembly manifestVersion="1.0" xmlns="urn:schemas-microsoft-com:asm.v1">' + [Environment]::NewLine + '  <assemblyIdentity type="win32" name="'+$item[1]+'" version="1.0.0.0" processorArchitecture="x86"/>' + [Environment]::NewLine + '  <application>' + [Environment]::NewLine + '    <windowsSettings>' + [Environment]::NewLine + '      <activeCodePage xmlns="http://schemas.microsoft.com/SMI/2019/WindowsSettings">UTF-8</activeCodePage>' + [Environment]::NewLine + '    </windowsSettings>' + [Environment]::NewLine + '  </application>' + [Environment]::NewLine + '</assembly>' + [Environment]::NewLine; [IO.File]::WriteAllText($dest,$xml,$utf8) }"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "$ErrorActionPreference='Stop'; $root=$env:AUDIOCHECKER_ROOT; $items=@(@('achkgui.exe.manifest','AudioChecker.UTF8.achkgui'),@('Codecs\APE\mac.exe.manifest','AudioChecker.UTF8.Codecs.APE.mac'),@('Codecs\Analyzer\aucdtect.exe.manifest','AudioChecker.UTF8.Codecs.Analyzer.aucdtect'),@('Codecs\FLAC\flac.exe.manifest','AudioChecker.UTF8.Codecs.FLAC.flac'),@('Codecs\LPAC\lpac.exe.manifest','AudioChecker.UTF8.Codecs.LPAC.lpac'),@('Codecs\SHN\shortn32.exe.manifest','AudioChecker.UTF8.Codecs.SHN.shortn32')); $utf8=New-Object Text.UTF8Encoding($false); foreach($item in $items){ $dest=Join-Path $root $item[0]; [IO.Directory]::CreateDirectory([IO.Path]::GetDirectoryName($dest)) | Out-Null; $xml='<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + [Environment]::NewLine + '<assembly manifestVersion="1.0" xmlns="urn:schemas-microsoft-com:asm.v1">' + [Environment]::NewLine + '  <assemblyIdentity type="win32" name="'+$item[1]+'" version="1.0.0.0" processorArchitecture="x86"/>' + [Environment]::NewLine + '  <application>' + [Environment]::NewLine + '    <windowsSettings>' + [Environment]::NewLine + '      <activeCodePage xmlns="http://schemas.microsoft.com/SMI/2019/WindowsSettings">UTF-8</activeCodePage>' + [Environment]::NewLine + '    </windowsSettings>' + [Environment]::NewLine + '  </application>' + [Environment]::NewLine + '</assembly>' + [Environment]::NewLine; [IO.File]::WriteAllText($dest,$xml,$utf8) }"
 if errorlevel 1 (
     echo.
     echo ERROR: Could not create one or more manifests.
@@ -30,46 +30,6 @@ echo.
 echo UTF-8 manifests installed. Restart AudioChecker and test a Unicode path.
 pause
 exit /b 0
-
-:EnsureWinget
-if errorlevel 1 (
-    echo ERROR: Dependency bootstrap failed.
-    pause
-    exit /b 1
-)
-
-set "BASE=https://raw.githubusercontent.com/karpuzikov/userscripts/main/audio-tools/audiochecker-utf8-patch"
-set /p "ROOT=Enter the folder containing achkgui.exe: "
-set "ROOT=%ROOT:"=%"
-if not exist "%ROOT%\achkgui.exe" (
-    echo achkgui.exe was not found in that folder.
-    pause
-    exit /b 1
-)
-
-for %%D in ("Codecs\APE" "Codecs\Analyzer" "Codecs\FLAC" "Codecs\LPAC" "Codecs\SHN") do mkdir "%ROOT%\%%~D" 2>nul
-call :get "achkgui.exe.manifest" "%ROOT%\achkgui.exe.manifest" || goto :download_failed
-call :get "Codecs/APE/mac.exe.manifest" "%ROOT%\Codecs\APE\mac.exe.manifest" || goto :download_failed
-call :get "Codecs/Analyzer/aucdtect.exe.manifest" "%ROOT%\Codecs\Analyzer\aucdtect.exe.manifest" || goto :download_failed
-call :get "Codecs/FLAC/flac.exe.manifest" "%ROOT%\Codecs\FLAC\flac.exe.manifest" || goto :download_failed
-call :get "Codecs/LPAC/lpac.exe.manifest" "%ROOT%\Codecs\LPAC\lpac.exe.manifest" || goto :download_failed
-call :get "Codecs/SHN/shortn32.exe.manifest" "%ROOT%\Codecs\SHN\shortn32.exe.manifest" || goto :download_failed
-
-echo.
-echo UTF-8 manifests installed. Restart AudioChecker and test a Unicode path.
-pause
-exit /b 0
-
-:get
-set "DL_URL=%BASE%/%~1"
-set "DL_DEST=%~2"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "$ErrorActionPreference='Stop'; $ProgressPreference='SilentlyContinue'; Invoke-WebRequest -UseBasicParsing -Uri $env:DL_URL -OutFile $env:DL_DEST"
-exit /b %ERRORLEVEL%
-
-:download_failed
-echo ERROR: A required manifest could not be downloaded.
-pause
-exit /b 1
 
 :EnsureWinget
 set "WINGET="
